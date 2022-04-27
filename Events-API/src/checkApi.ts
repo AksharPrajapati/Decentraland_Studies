6144a06caea5800c8b874cc4b38471549949c132
Akshar Prajapati <aksharpp369@gmail.com>
2022-04-27 10:21:10 +0530

    integrate decetraland event api

---
+++ Events-API/src/checkApi.ts
@@ -0,0 +1,24 @@
+import { displayEvent } from "./eventBoard";
+
+export async function getEvents() {
+  const events: any[] = [];
+  const url = "https://events.decentraland.org/api/events/?limit=5";
+
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+
+    log("JOSN......", json);
+
+    for (const event of json.data) {
+      if (event.live) {
+        events.push(event);
+      }
+    }
+
+    log(events);
+    return json.data;
+  } catch (e) {
+    log("error getting event data ", e);
+  }
+}
---
+++ Events-API/src/game.ts
@@ -0,0 +1,6 @@
+import { displayEvent, createEventsBoard } from "./eventBoard";
+
+createEventsBoard({
+  position: new Vector3(8, 3.5, 8),
+  rotation: Quaternion.Euler(0, 225, 0),
+}).catch((error) => log(error));
